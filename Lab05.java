af3d350b9df602b11dca23ba9c0ba7912b6348a7
JackNWthepbfantic99 <staceynavin@Jacks-MacBook-Air-2.local>
2014-11-11 14:41:40 +0000

    ran race

--- Lab05.java
+++ Lab05.java
@@ -2,16 +2,25 @@
 *
 * Description of the program goes here  // provide a brief description
 *
-* @author <Your Name here>  // replace <...> with your name
-* @version <date/of/completion> // replace <...> with the date
+* @author Jack Navin-Weinstein  // replace <...> with your name
+* @version 08/11/14 // replace <...> with the date
 */
 
 import edu.fcps.karel2.Display;
 import edu.fcps.karel2.Robot;
 
 public class Lab05 {
-
+     
      public static void main(String[] args) {
-          // TODO Your code goes here
+          Display.openWorld("maps/shuttle.map");
+          Display.setSize(10, 10);
+          Display.setSpeed(8);
+          Racer Joe = new Racer(1);
+          Racer Jack = new Racer(4);
+          Racer Sam = new Racer(7);
+          Joe.runTheRace();
+          Jack.runTheRace();
+          Sam.runTheRace();
+          
      }
 }
